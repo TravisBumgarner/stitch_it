d310c86c23d16175274fbc1702e157d65551f7a2
Travis Bumgarner <travis-bumgarner@pluralsight.com>
2020-02-20 13:37:42 -0500

    Added file uploads

---
+++ README.md
@@ -0,0 +1,10 @@
+# Resources
+
+## Flask
+
+https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iii-web-forms
+
+## Flask File Uploads
+
+https://www.javatpoint.com/flask-file-uploading
+https://riptutorial.com/flask/example/19418/save-uploads-on-the-server
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
+import os
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request
 from app.forms import LoginForm
 
 @app.route('/')
@@ -7,6 +8,7 @@ from app.forms import LoginForm
 def index():
     user = {'username': 'Travis'}
     return render_template('index.html', title='Home', user=user)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -16,3 +18,12 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/success', methods = ['POST'])  
+def success():  
+    if request.method == 'POST':  
+        f = request.files['file']  
+        print(os.path.abspath(__file__))
+        f.save(os.path.join('uploads', f.filename))
+        return render_template("success.html", name = f.filename)  
