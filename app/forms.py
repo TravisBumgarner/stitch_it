03e763f03c741b8ea026882ef967f2dfdc577128
Travis Bumgarner <travis-bumgarner@pluralsight.com>
2020-02-25 13:43:21 -0500

    Got rid of login

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, FileField, IntegerField, RadioField
+from wtforms import SubmitField, FileField, IntegerField, RadioField
 from wtforms.validators import DataRequired
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
 
 
 class StitchForm(FlaskForm):
--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,10 @@
 import os
 from app import app
 from flask import render_template, flash, redirect, request
-from app.forms import LoginForm, StitchForm
+from app.forms import StitchForm
 from app.stitch import stitch_image
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Travis'}
-    return render_template('index.html', title='Home', user=user)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title='Sign In', form=form)
-
-
-@app.route('/stitch', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def stitch():
     form = StitchForm()
     return render_template('stitch.html', title='Stitch', form=form)
